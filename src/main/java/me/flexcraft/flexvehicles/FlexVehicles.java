94e7a1823e5decc828b91b731a5e61ce058b26c0
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-31 08:21:22 +0100

    Update FlexVehicles.java

--- src/main/java/me/flexcraft/flexvehicles/FlexVehicles.java
+++ src/main/java/me/flexcraft/flexvehicles/FlexVehicles.java
@@ -2,43 +2,30 @@ package me.flexcraft.flexvehicles;
 
 import me.flexcraft.flexvehicles.listeners.VehicleListener;
 import me.flexcraft.flexvehicles.vehicle.VehicleManager;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import net.milkbowl.vault.economy.Economy;
-
 public final class FlexVehicles extends JavaPlugin {
 
     private static Economy econ;
-    private VehicleManager vehicleManager;
+    private VehicleManager manager;
 
     @Override
     public void onEnable() {
         if (!setupEconomy()) {
-            getLogger().severe("Vault не найден! Плагин выключен.");
+            getLogger().severe("Vault not found!");
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
 
-        vehicleManager = new VehicleManager(this);
-
-        Bukkit.getPluginManager().registerEvents(
-                new VehicleListener(vehicleManager),
-                this
-        );
-
-        getCommand("vehicle").setExecutor(new VehicleCommand());
-
-        getLogger().info("FlexVehicles enabled!");
-    }
-
-    public static Economy getEconomy() {
-        return econ;
+        manager = new VehicleManager();
+        Bukkit.getPluginManager().registerEvents(new VehicleListener(manager), this);
+        getCommand("vehicle").setExecutor(new VehicleCommand(econ));
     }
 
     private boolean setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) return false;
         RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
         if (rsp == null) return false;
         econ = rsp.getProvider();
