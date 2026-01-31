492882640d2b76454e9f4ecd5ff6b1cb27c0d598
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-31 08:02:39 +0100

    Create EconomyManager.java

---
+++ economy/EconomyManager.java
@@ -0,0 +1,26 @@
+package me.flexcraft.flexvehicles.economy;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+public class EconomyManager {
+
+    private static Economy econ = null;
+
+    public static boolean setupEconomy() {
+        if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp =
+                Bukkit.getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) return false;
+
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public static Economy getEconomy() {
+        return econ;
+    }
+}
