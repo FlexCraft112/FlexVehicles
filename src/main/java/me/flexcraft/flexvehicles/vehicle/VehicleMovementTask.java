879e1de23ff6629caca0f01f6f0284101abcb8e6
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-31 07:46:53 +0100

    Update VehicleMovementTask.java

--- src/main/java/me/flexcraft/flexvehicles/vehicle/VehicleMovementTask.java
+++ src/main/java/me/flexcraft/flexvehicles/vehicle/VehicleMovementTask.java
@@ -3,29 +3,41 @@ package me.flexcraft.flexvehicles.vehicle;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
-public class VehicleMovementTask extends BukkitRunnable {
+public class VehicleMovementTask implements Runnable {
 
-    private final VehicleManager manager;
+    private final VehicleManager vehicleManager;
+    private final JavaPlugin plugin;
 
-    public VehicleMovementTask(VehicleManager manager) {
-        this.manager = manager;
+    public VehicleMovementTask(JavaPlugin plugin, VehicleManager vehicleManager) {
+        this.plugin = plugin;
+        this.vehicleManager = vehicleManager;
+
+        Bukkit.getScheduler().runTaskTimer(plugin, this, 0L, 1L);
     }
 
     @Override
     public void run() {
         for (Player player : Bukkit.getOnlinePlayers()) {
-            if (!manager.hasVehicle(player)) continue;
+            if (!vehicleManager.hasVehicle(player)) continue;
 
-            ArmorStand stand = manager.getVehicle(player);
-            if (stand == null) continue;
+            ArmorStand stand = vehicleManager.getVehicle(player);
+            if (stand == null || stand.isDead()) continue;
 
-            Vector dir = player.getLocation().getDirection().normalize();
-            Vector velocity = dir.multiply(0.6);
+            // направление взгляда игрока
+            Vector direction = player.getLocation().getDirection().normalize();
 
+            // скорость
+            double speed = 0.6;
+
+            // движение
+            Vector velocity = direction.multiply(speed);
             stand.teleport(stand.getLocation().add(velocity));
+
+            // поворот транспорта
+            stand.setRotation(player.getLocation().getYaw(), 0);
         }
     }
 }
