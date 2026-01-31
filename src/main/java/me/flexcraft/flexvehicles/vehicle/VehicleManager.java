11ac38874906debbd4066efc9a7119547ecff4f3
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-31 08:08:24 +0100

    Update VehicleManager.java

--- src/main/java/me/flexcraft/flexvehicles/vehicle/VehicleManager.java
+++ src/main/java/me/flexcraft/flexvehicles/vehicle/VehicleManager.java
@@ -1,19 +1,53 @@
-public void spawnVehicle(Player player) {
-    if (activeVehicles.containsKey(player.getUniqueId())) return;
+package me.flexcraft.flexvehicles.vehicle;
 
-    Location loc = player.getLocation();
-    ArmorStand stand = loc.getWorld().spawn(loc, ArmorStand.class);
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
-    stand.setInvisible(true);
-    stand.setInvulnerable(true);
-    stand.setGravity(false);
-    stand.setCustomName("flex_heli");
-    stand.addPassenger(player);
+import java.util.HashMap;
+import java.util.UUID;
 
-    activeVehicles.put(player.getUniqueId(), stand);
+public class VehicleManager {
 
-    new VehicleMovementTask(player, stand).runTaskTimer(
-        Bukkit.getPluginManager().getPlugin("FlexVehicles"),
-        0L, 1L
-    );
+    private final HashMap<UUID, ArmorStand> activeVehicles = new HashMap<>();
+    private final JavaPlugin plugin;
+
+    public VehicleManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void spawnVehicle(Player player) {
+        if (activeVehicles.containsKey(player.getUniqueId())) return;
+
+        Location loc = player.getLocation();
+        ArmorStand stand = loc.getWorld().spawn(loc, ArmorStand.class);
+
+        stand.setInvisible(true);
+        stand.setInvulnerable(true);
+        stand.setGravity(false);
+        stand.setMarker(true);
+        stand.setCustomName("flex_vehicle");
+
+        stand.addPassenger(player);
+        activeVehicles.put(player.getUniqueId(), stand);
+
+        new VehicleMovementTask(player, stand).runTaskTimer(plugin, 0L, 1L);
+    }
+
+    public void removeVehicle(Player player, boolean giveBackItem) {
+        UUID id = player.getUniqueId();
+        if (!activeVehicles.containsKey(id)) return;
+
+        ArmorStand stand = activeVehicles.get(id);
+        stand.remove();
+
+        activeVehicles.remove(id);
+    }
+
+    public boolean hasVehicle(Player player) {
+        return activeVehicles.containsKey(player.getUniqueId());
+    }
 }
