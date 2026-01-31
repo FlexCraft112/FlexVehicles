961c7317874f4c1baa2b527cde9b52479e4d9b13
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-31 07:49:29 +0100

    Update FlexVehicles.java

--- src/main/java/me/flexcraft/flexvehicles/FlexVehicles.java
+++ src/main/java/me/flexcraft/flexvehicles/FlexVehicles.java
@@ -1,25 +1,31 @@
 package me.flexcraft.flexvehicles;
 
-import org.bukkit.plugin.java.JavaPlugin;
 import me.flexcraft.flexvehicles.listeners.VehicleListener;
 import me.flexcraft.flexvehicles.vehicle.VehicleManager;
+import me.flexcraft.flexvehicles.vehicle.VehicleMovementTask;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class FlexVehicles extends JavaPlugin {
 
-    private static FlexVehicles instance;
     private VehicleManager vehicleManager;
 
     @Override
     public void onEnable() {
-        instance = this;
-        vehicleManager = new VehicleManager();
+        saveDefaultConfig();
 
-        getServer().getPluginManager().registerEvents(new VehicleListener(vehicleManager), this);
+        vehicleManager = new VehicleManager(this);
+        new VehicleMovementTask(this, vehicleManager);
+
+        getServer().getPluginManager().registerEvents(
+                new VehicleListener(vehicleManager),
+                this
+        );
 
         getLogger().info("FlexVehicles enabled");
     }
 
-    public static FlexVehicles getInstance() {
-        return instance;
+    @Override
+    public void onDisable() {
+        getLogger().info("FlexVehicles disabled");
     }
 }
