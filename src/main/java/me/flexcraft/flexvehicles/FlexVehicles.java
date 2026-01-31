e1fff3d53709b5403cc8207ea8deb489cbac1f9b
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-31 08:14:00 +0100

    Update FlexVehicles.java

--- src/main/java/me/flexcraft/flexvehicles/FlexVehicles.java
+++ src/main/java/me/flexcraft/flexvehicles/FlexVehicles.java
@@ -3,14 +3,24 @@ package me.flexcraft.flexvehicles;
 import me.flexcraft.flexvehicles.listeners.VehicleListener;
 import me.flexcraft.flexvehicles.vehicle.VehicleManager;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.milkbowl.vault.economy.Economy;
 
 public final class FlexVehicles extends JavaPlugin {
 
+    private static Economy econ;
     private VehicleManager vehicleManager;
 
     @Override
     public void onEnable() {
+        if (!setupEconomy()) {
+            getLogger().severe("Vault не найден! Плагин выключен.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
         vehicleManager = new VehicleManager(this);
 
         Bukkit.getPluginManager().registerEvents(
@@ -18,10 +28,20 @@ public final class FlexVehicles extends JavaPlugin {
                 this
         );
 
+        getCommand("vehicle").setExecutor(new VehicleCommand());
+
         getLogger().info("FlexVehicles enabled!");
     }
 
-    public VehicleManager getVehicleManager() {
-        return vehicleManager;
+    public static Economy getEconomy() {
+        return econ;
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) return false;
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) return false;
+        econ = rsp.getProvider();
+        return econ != null;
     }
 }
